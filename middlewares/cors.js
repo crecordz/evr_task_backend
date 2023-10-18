4f97b9d5ed54ff64059825ee358306475ada17d7
crecordz <s014451@mail.ru>
2023-10-18 17:24:20 +0300

    fix: deploy cors2

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,6 @@
 const allowedCors = [
   'http://localhost:5173',
-  'https://cheery-cupcake-ff41dc.netlify.app/',
+  'https://sensational-belekoy-5442d3.netlify.app',
 ];
 
 module.exports = (req, res, next) => {
